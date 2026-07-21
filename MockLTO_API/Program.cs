47663101d3a78a72f261e0da63332a7121d47dee
rj-sr <romarijoyrefamonte@gmail.com>
2026-07-22 00:46:35 +0800

    Add SQL helpers and MVC structure

--- MockLTO_API/Program.cs
+++ MockLTO_API/Program.cs
@@ -1,3 +1,5 @@
+using MockLTO_API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ISqlStoredProcedureExecutor, SqlStoredProcedureExecutor>();
 
 var app = builder.Build();
 
